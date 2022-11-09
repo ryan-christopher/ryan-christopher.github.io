233d2eca0c77d81d75356e37e1316ccea627190e
ryan-christopher <rechristopher99@gmail.com>
2022-11-09 00:42:12 -0500

    added vercel analytics

--- current/src/App.js
+++ current/src/App.js
@@ -7,10 +7,12 @@ import Navbar from './components/Navbar'
 import './styles/App.css';
 import React from 'react'
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <ParallaxProvider>
+      <Analytics />
       <div className="App bg-white dark:bg-black ease-in duration-200">
         <Darkmode />
         <Navbar />
