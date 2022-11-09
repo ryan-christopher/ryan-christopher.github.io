9d8e32db14e6d1088c0571e64804253365298fcc
ryan-christopher <rechristopher99@gmail.com>
2022-11-08 22:48:26 -0500

    mobile spacing fixes

--- current/src/components/SectionHeader.js
+++ current/src/components/SectionHeader.js
@@ -15,7 +15,7 @@ function SectionHeader({ title }) {
                     }}
                     viewport={{ once: true }}>
                     <h2 className='text-left w-fit overflow-hidden uppercase pr-[7px] mt-[50px] ml-[20px] sm:ml-[100px] md:ml-[200px] rounded-[10px] 
-                    font-[Takota] text-[#171717] text-[55px] md:text-[60px] lg:text-[85px] xl:text-[125px] dark:text-[#f9f5e9] ease-in duration-200'>
+                    font-[Takota] text-[#171717] text-[45px] sm:text-[50px] md:text-[60px] lg:text-[85px] xl:text-[125px] dark:text-[#f9f5e9] ease-in duration-200'>
                         {title}
                     </h2>
                 </motion.div>
